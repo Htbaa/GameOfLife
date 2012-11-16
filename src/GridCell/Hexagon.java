3ba48e6495e9090dc4fc3440aa156c1cfd8c5123
Christiaan Kras <c.kras@pcc-online.net>
2012-11-16 16:49:54 +0100

    Corrected Hexagon coordinates

--- src/GridCell/Hexagon.java
+++ src/GridCell/Hexagon.java
@@ -49,13 +49,13 @@ public class Hexagon implements Profile {
     public List<Point> GetNeighbours(Point position) {
         List<Point> neighbours = new ArrayList<Point>();
         
-        neighbours.add(new Point(-1, -1)); // left
+        neighbours.add(new Point(-1, 1)); // left
         neighbours.add(new Point(-1, 0));
         
         neighbours.add(new Point(0, -1)); // top
         neighbours.add(new Point(0, 1)); // bottom
         
-        neighbours.add(new Point(1, -1)); // right
+        neighbours.add(new Point(1, 1)); // right
         neighbours.add(new Point(1, 0));
         
         return neighbours;
