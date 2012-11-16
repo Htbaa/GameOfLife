6d531704955cb7b1aaac827a87b017bded75b5fe
Christiaan Kras <c.kras@pcc-online.net>
2012-11-16 01:23:28 +0100

    Filled squares

--- src/GridCell/Square.java
+++ src/GridCell/Square.java
@@ -20,6 +20,8 @@ public class Square implements Profile {
     @Override
     public void Draw(Graphics g, int x, int y, double size) {
         Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.black);
+        g2.fillRect(x * (int)size, y * (int)size, (int)size, (int)size);
         g2.setColor(Color.red);
         g2.drawRect(x * (int)size, y * (int)size, (int)size, (int)size);
         
