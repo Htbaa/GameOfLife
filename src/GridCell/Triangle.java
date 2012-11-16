3fb509be0bb30e658c18bb9b6e967be36d2f0c36
Christiaan Kras <c.kras@pcc-online.net>
2012-11-16 01:32:24 +0100

    Filled triangles

--- src/GridCell/Triangle.java
+++ src/GridCell/Triangle.java
@@ -8,6 +8,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Polygon;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,19 +24,28 @@ public class Triangle implements Profile {
         boolean isOdd = x % 2 == 0;
         
         x = x/2;
-
         int base_x = x * (int)size, base_y = y * (int)size;
+        
+        Point p1,p2,p3;
         g2.setColor(Color.red);
         if(isOdd) {    
-            g2.drawLine(base_x, base_y, base_x + (int)size, base_y);
-            g2.drawLine(base_x, base_y, base_x, base_y + (int)size);
-            g2.drawLine(base_x, base_y + (int)size, base_x + (int)size, base_y);
+            p1 = new Point(base_x, base_y);
+            p2 = new Point(base_x + (int)size, base_y);
+            p3 = new Point(base_x, base_y + (int)size);
         }
         else {
-            g2.drawLine(base_x, base_y + (int)size, base_x + (int)size, base_y + (int)size);
-            g2.drawLine(base_x + (int)size, base_y, base_x, base_y + (int)size);
-            g2.drawLine(base_x + (int)size, base_y, base_x + (int)size, base_y + (int)size);
+            p1 = new Point(base_x, base_y + (int)size);
+            p2 = new Point(base_x + (int)size, base_y);
+            p3 = new Point(base_x + (int)size, base_y + (int)size);
         }
+
+        int[] xs = { p1.x, p2.x, p3.x };
+        int[] ys = { p1.y, p2.y, p3.y };
+        Polygon triangle = new Polygon(xs, ys, xs.length);
+        g2.setColor(Color.black);
+        g2.fillPolygon(triangle);
+        g2.setColor(Color.red);
+        g2.drawPolygon(triangle);
     }
 
     @Override
